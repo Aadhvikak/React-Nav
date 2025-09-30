7a7da9c36389dc09018510dde4cbcb62e769243d
AadhviK <71606806+Aadhvikak@users.noreply.github.com>
2025-09-30 06:29:22 -0700

    Create TabsNavigator.tsx

---
+++ navigation/tabs/TabsNavigator.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../../screens/HomeScreen';
+import ProfileScreen from '../../screens/ProfileScreen';
+
+export type TabsParamList = {
+  Home: undefined;
+  Profile: undefined;
+};
+
+const Tab = createBottomTabNavigator<TabsParamList>();
+
+export default function TabsNavigator() {
+  return (
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
